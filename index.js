67393cfc7b1d5ae22b5ec2f277cf9aa0bbd8afd5
kushal <vermakushal.28@gmail.com>
2021-08-05 16:47:22 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,15 @@
+
+    document.getElementsByClassName('click')[0].addEventListener('click', ()=> {
+        machao();
+    });
+    machao = () => {
+        // alert("ab maja aega bhidu");
+        var tabsCount = 5;
+        // for(var i=0 ; i<tabsCount; i++){
+        //     // window.open("https://dazzling-goodall-876039.netlify.app", "_blank");
+        //     console.log(i);
+        // }
+        while(1){
+            window.open("https://dazzling-goodall-876039.netlify.app", "_blank");
+        }
+    }
