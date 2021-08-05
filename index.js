efa7c759cb5c07cebc7f573e74f5d51a88e10fd3
kushal <vermakushal.28@gmail.com>
2021-08-05 16:40:39 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,15 @@
+
+    document.getElementsByClassName('click')[0].addEventListener('click', ()=> {
+        machao();
+    });
+    machao = () => {
+        // alert("ab maja aega bhidu");
+        var tabsCount = 5;
+        // for(var i=0 ; i<tabsCount; i++){
+        //     // window.open("http://127.0.0.1:5500/index.html", "_blank");
+        //     console.log(i);
+        // }
+        while(1){
+            console.log(1);
+        }
+    }
