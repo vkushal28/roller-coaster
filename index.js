8d920dc5a4152d5677e9f544007eb67fbcb69fe9
kushal <vermakushal.28@gmail.com>
2021-08-05 16:48:09 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,12 +3,7 @@
         machao();
     });
     machao = () => {
-        // alert("ab maja aega bhidu");
-        var tabsCount = 5;
-        // for(var i=0 ; i<tabsCount; i++){
-        //     // window.open("https://dazzling-goodall-876039.netlify.app", "_blank");
-        //     console.log(i);
-        // }
+        alert("ab maja aega bhidu");
         while(1){
             window.open("https://dazzling-goodall-876039.netlify.app", "_blank");
         }
